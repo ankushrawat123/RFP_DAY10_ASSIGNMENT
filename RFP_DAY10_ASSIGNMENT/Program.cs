924637b095b8d4dc4eb1c1ebb645b74311a864b6
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-28 00:06:13 +0530

    Uploading WageUc file

--- RFP_DAY10_ASSIGNMENT/Program.cs
+++ RFP_DAY10_ASSIGNMENT/Program.cs
@@ -1,8 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using RFP_DAY10_ASSIGNMENT;
-Console.WriteLine(" \nWelcome to Employee Attendance Program .............................................");
+
 int Is_Full_Time = 1;
-Random obj = new Random();
-int num = obj.Next(0, 2);
-Attendance obj3 = new Attendance();
-obj3.attendanceMethod(Is_Full_Time, num);
+int Is_Part_Time = 2;
+int Emp_Rate_Per_Hour = 20;
+Console.WriteLine("Enter \n1 for ATTENDANCE PROGRAM \n2 for WAGE PROGRAM ");
+int Option = Convert.ToInt32(Console.ReadLine());
+switch (Option)
+{
+    case 1:
+        Console.WriteLine(" \n Welcome to Employee Attendance Program .............................................");
+        Random obj = new Random();
+        int num = obj.Next(0, 2);
+        Attendance obj1 = new Attendance();
+        obj1.attendanceMethod(Is_Full_Time, num);
+        break;
+
+    case 2:
+        WageUc obj2 = new WageUc();
+        obj2.Wage(Is_Full_Time, Emp_Rate_Per_Hour);
+        break;
+}
