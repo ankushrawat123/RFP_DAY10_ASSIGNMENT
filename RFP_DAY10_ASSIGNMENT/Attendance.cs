5f03e6928700c4ea137ebcdf0c963faa23451961
ankushrawat123 <ankushrwt344@gmail.com>
2022-04-27 23:42:32 +0530

    uploading attendance file

---
+++ RFP_DAY10_ASSIGNMENT/Attendance.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_DAY10_ASSIGNMENT
+{
+    internal class Attendance
+    {
+        public void attendanceMethod(int Is_Full_Time, int num)
+        {
+
+            if (num == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is absent");
+            }
+
+        }
+    }
+}
--- RFP_DAY10_ASSIGNMENT/Program.cs
+++ RFP_DAY10_ASSIGNMENT/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RFP_DAY10_ASSIGNMENT;
+Console.WriteLine(" \nWelcome to Employee Attendance Program .............................................");
+int Is_Full_Time = 1;
+Random obj = new Random();
+int num = obj.Next(0, 2);
+Attendance obj3 = new Attendance();
+obj3.attendanceMethod(Is_Full_Time, num);
