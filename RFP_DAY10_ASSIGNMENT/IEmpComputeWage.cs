dba1094b2eb33c004a0bc1548057e13c2bfac794
AnkushRawat <ankushrwt344@gmail.com>
2022-05-04 13:30:59 +0530

    Uploading interface, empWageBuilderArray,IempComputeWage file in EmpWageBuilderArrayUc10 branch

---
+++ RFP_DAY10_ASSIGNMENT/IEmpComputeWage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_DAY10_ASSIGNMENT
+{
+    internal interface IEmpComputeWage
+    {
+        public void addCompanyEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth);
+    }
+}
---
+++ RFP_DAY10_ASSIGNMENT/interface.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RFP_DAY10_ASSIGNMENT
+{
+    public class @interface
+    {
+
+        private string company;
+        private int empRatePerHour;
+        private int numOfWorkingDays;
+        private int maxHoursPerMonth;
+        private int totalEmpWage;
+
+
+        public @interface(String company, int empRatePerHour, int numOfWorkingDays, int maxHourPerMonth)
+        {
+
+            this.company = company;
+            this.empRatePerHour = empRatePerHour;
+            this.numOfWorkingDays = numOfWorkingDays;
+            this.maxHoursPerMonth = maxHourPerMonth;
+        }
+
+        public void setTotalEmpWage(int totalEmpWage)
+        {
+            this.totalEmpWage = totalEmpWage;
+        }
+        public string toString()
+        {
+            return "Total Emp Wage for company :" + this.company + "is : " + this.totalEmpWage;
+        }
+
+    }
+}
